0aac1f503b2d9a17f5ffb7ce2b211aabc0372d01
marcowang-0423 <m0983557762@gmail.com>
2021-04-04 21:54:35 +0800

    cookie-add

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -1,39 +1,36 @@
-import { useContext } from "react";
-import { Button, notification } from "antd"
-import { StoreContext } from "../store"
-import { CART_ADD_ITEM } from "../utils/constants"
+import { useContext, useEffect } from "react";
+import { Button, notification } from "antd";
+import { StoreContext } from "../store";
+import { CART_ADD_ITEM } from "../utils/constants";
 import { CartIcon } from "./Icons";
+import { addCartItem } from "../actions";
 
 export default function AddToCart({ product, qty }) {
-  const { dispatch } = useContext(StoreContext);
+  const {
+    state: { cartItems },
+    dispatch,
+  } = useContext(StoreContext);
 
   const openNotification = () => {
     notification.open({
-      message: 'Shopping Notification',
-      description:
-        `${qty} ${qty > 1 ? "pieces" : "piece"} of ${product.name} ${qty > 1 ? "have" : "has"} been added to your cart.`,
+      message: "Shopping Notification",
+      description: `${qty} ${qty > 1 ? "pieces" : "piece"} of ${product.name} ${
+        qty > 1 ? "have" : "has"
+      } been added to your cart.`,
       onClick: () => {
-        console.log('Notification Clicked!');
+        console.log("Notification Clicked!");
       },
-      placement: 'bottomRight'
+      placement: "bottomRight",
     });
   };
 
   const addToCart = () => {
     openNotification();
-    dispatch({
-      type: CART_ADD_ITEM,
-      payload: {
-        id: product.id,
-        name: product.name,
-        image: product.image,
-        price: product.price,
-        countInStock: product.countInStock,
-        qty,
-      },
-    });
+    addCartItem(dispatch, product, qty);
   };
-
+  useEffect(() => {
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }, [cartItems]);
 
   return (
     <Button type="primary" className="btn-tocar" onClick={addToCart}>
